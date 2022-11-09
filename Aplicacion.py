9167bfd4c98b8633865eac79257aaca1a0c0ed8b
Daniel Gonzales <2017110596@untels.edu.pe>
2022-11-09 09:01:18 -0500

    Parte 02 - Realizado por Daniel Alexander Gonzales Castillo

--- Aplicacion.py
+++ Aplicacion.py
@@ -4,6 +4,19 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import sqlite3
+conexion = sqlite3.connect("BarzolaGonzalesSoto_almacen")
+
+tabla_producto = """CREATE TABLE Producto(
+        idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+        codigo NUMBER UNIQUE,
+        nombre TEXT,
+        precio REAL
+        )
+        """
+
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
 
 def menu():
     print("\n\t\Menú Opciones:")
